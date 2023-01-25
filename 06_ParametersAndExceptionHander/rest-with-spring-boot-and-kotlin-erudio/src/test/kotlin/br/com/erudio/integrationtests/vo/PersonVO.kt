949f15ae464dbb30c41e9dd98fb73068bc134236
Richart Rupolo <richart.rupolo@hotmail.com>
2023-01-24 21:40:40 -0300

    ajustes

---
+++ 06_ParametersAndExceptionHander/rest-with-spring-boot-and-kotlin-erudio/src/test/kotlin/br/com/erudio/integrationtests/vo/PersonVO.kt
@@ -0,0 +1,9 @@
+package br.com.erudio.integrationtests.vo;
+
+public class PersonVO (
+    var id: Long = 0,
+    var firstName: String = "",
+    var lastName: String = "",
+    var address: String = "",
+    var gender: String = ""
+)
